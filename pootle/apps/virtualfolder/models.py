4a07c1eef8373ef6369c5d98b112065ff398520f
Leandro Regueiro <leandro.regueiro@gmail.com>
2015-01-27 14:42:53 +0100

    VFolder: Retrieve applicable vfolders in a given path
    Part of #3522.

--- pootle/apps/virtualfolder/models.py
+++ pootle/apps/virtualfolder/models.py
@@ -18,10 +18,67 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, see <http://www.gnu.org/licenses/>.
 
+import re
+
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 from pootle.core.markup import get_markup_filter_name, MarkupField
+
+
+def pattern_for_path(pootle_path):
+    """Return a regular expression to match Virtual Folders locations.
+
+    Given a pootle path like /gl/firefox/browser/chrome/ this function returns
+    a regular expression to filter all the existing virtual folders to only
+    retrieve the ones that are applicable in that pootle path.
+
+    For the pootle path specified above the applicable virtual folders are the
+    ones with locations matching the provided pootle path, and also the pootle
+    path for any upper directory. Also it matches any location that includes
+    the {LANG} or {PROJ} placeholders in any combination. Those virtual folders
+    whose location starts with /projects/ and have the same path are also
+    returned.
+    """
+    def pattern_for_sublist(sublist):
+        if len(sublist) == 1:
+            return "".join([
+                "(/",
+                sublist[0],
+                ")?",
+            ])
+
+        return "".join([
+            "(/",
+            sublist[0],
+            pattern_for_sublist(sublist[1:]),
+            ")?",
+        ])
+
+    items = pootle_path.strip("/").split("/")
+    str_list = [
+        "^/((",
+        items[0],
+        "|\{LANG\}|projects)",
+    ]
+
+    if len(items) > 1:
+        str_list += [
+            "|((",
+            items[0],
+            "|\{LANG\}|projects)(/(",
+            items[1],
+            "|{PROJ})",
+        ]
+
+        if len(items) > 2:
+            str_list += pattern_for_sublist(items[2:])
+
+        str_list.append(")?)")
+
+    str_list.append(")/$")
+
+    return "".join(str_list)
 
 
 class VirtualFolder(models.Model):
@@ -64,3 +121,24 @@ class VirtualFolder(models.Model):
 
     def __unicode__(self):
         return ": ".join([self.name, self.location])
+
+    @classmethod
+    def get_applicable_for(cls, pootle_path):
+        """Return the applicable virtual folders in the given pootle path.
+
+        Given a pootle path like /gl/firefox/browser/chrome/ this method
+        returns the virtual folders whose location matches.
+
+        For the pootle path specified above the applicable virtual folders are
+        the ones with locations matching the provided pootle path, and also the
+        pootle path for any upper directory. Also it matches any location that
+        includes the {LANG} or {PROJ} placeholders in any combination. Those
+        virtual folders whose location starts with /projects/ and have the same
+        path are also returned.
+        """
+        vf_re = re.compile(pattern_for_path(pootle_path))
+
+        #TODO consider passing the regex directly to the ORM. See
+        # https://docs.djangoproject.com/en/1.7/ref/models/querysets/#regex
+        return [vf for vf in VirtualFolder.objects.filter(is_browsable=True)
+                if vf_re.match(vf.location)]
